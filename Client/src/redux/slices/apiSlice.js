dcf63c880011d8e64fed6d0911878d9a0a42c278
AK <aakhilp1@gmail.com>
2026-03-16 21:38:23 +0530

    Redux is implemented

---
+++ Client/src/redux/slices/apiSlice.js
@@ -0,0 +1,13 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+// const API_URL = import.meta.env.VITE_APP_BASE_URL + "/api";
+const API_URL = "/api";
+
+const baseQuery = fetchBaseQuery({ baseUrl: API_URL, credentials: "include"});
+
+export const apiSlice = createApi({
+  reducerPath: "api",
+  baseQuery,
+  tagTypes: ["user", "task"],
+  endpoints: (builder) => ({}),
+});
